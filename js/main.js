1308c5e8ac58e00cb18f9b33b983db6ac0447a01
Olezha83 <oleg.it.workbox@gmail.com>
2022-10-10 21:58:28 +0300

    files added

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const [inputText, checkButton, positiveAnswer, negativeAnswer] = [
+  'input_text',
+  'check_button',
+  'positive_answer',
+  'negative_answer',
+].map((id) => document.getElementById(id))
+
+function isPalindrome() {
+  const checkResult =
+    inputText.value === inputText.value.split('').reverse().join('')
+
+  checkResult
+    ? positiveAnswer.classList.add('answer_visible')
+    : negativeAnswer.classList.add('answer_visible')
+}
+
+inputText.addEventListener('focus', (event) => {
+  event.target.classList.remove('error')
+  event.target.value = ''
+  positiveAnswer.classList.remove('answer_visible')
+  negativeAnswer.classList.remove('answer_visible')
+})
+
+checkButton.addEventListener('click', () => {
+  inputText.value ? isPalindrome() : inputText.classList.add('error')
+})
