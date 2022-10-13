f038aa6e88be06673ddd8eb15c9023f3789f4acf
Olezha83 <oleg.it.workbox@gmail.com>
2022-10-13 20:10:24 +0300

    regular expression to remove punctuation added

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@ const [inputText, checkButton, positiveAnswer, negativeAnswer] = [
 ].map((id) => document.getElementById(id))
 
 function isPalindrome() {
+  const removedSymbols =
+    /[\u0020-\u0022\u0027-\u0029\u002C-\u002E\u003A\u003B\u003F\u00AB\u00BB\u2013\u2014\u2018\u2019\u201C\u201D]/g
   const checkResult =
-    inputText.value.toLowerCase().replaceAll(' ', '') ===
+    inputText.value.toLowerCase().replaceAll(removedSymbols, '') ===
     inputText.value
       .toLowerCase()
-      .replaceAll(' ', '')
+      .replaceAll(removedSymbols, '')
       .split('')
       .reverse()
       .join('')
